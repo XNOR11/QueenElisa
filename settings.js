133073104b69d37b0717c730fb10a9981821449e
X.N.O.R Education <adithyashavinda2005@gmail.com>
2024-09-21 09:31:05 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,8 +19,8 @@ module.exports = {
 
 Qᴜᴇᴇɴ ᴇʟɪꜱᴀ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ`,
   ALIVE_MEDIA: 'https://telegra.ph/file/d842677f522b6bef4dfe6.jpg',
-  MONGODB_URL: '', 
-  PREFIX: '.$&',
+  MONGODB_URL: 'en3m4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', 
+  PREFIX: '.',
   LANGUAGE: 'EN',
   PORT: 3000,
   BOT_NAME: '𝐐𝐔𝐄𝐄𝐍 𝐄𝐋𝐈𝐒𝐀',
